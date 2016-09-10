471f35443f66dc85a49e86bacb59800aba454e80
zhouyuexie <zhouyuexie@gmail.com>
2016-09-10 15:14:44 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,88 @@
-# response-iframe
-Responsive iframe
+# Responsive iframe
+
+Make your iframe response in Mobile and desktop.
+
+Example:
+
+```javascript
+var iframe = responseIframe("#response","youUrl");
+```
+
+## Install
+
+```html
+<div id="response"></div>
+```
+
+## Usage
+
+In your HTML:
+
+```html
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>usage</title>
+  <style type="text/css">
+    body{
+      margin:0;
+      padding:0;
+    }
+  </style>
+</head>
+<body>
+  <div id="response"></div>
+  <script type="text/javascript" src="dist/response-iframe.js"></script>
+</body>
+</html>
+```
+
+```javascript
+var iframe = responseIframe("#response","youUrl");
+```
+
+## More option
+
+You can use this method to create iframe:
+
+```html
+<iframe id="response"></iframe>
+```
+
+```javascript
+var iframe = responseIframe("#response","youUrl",{hasIframe:true});
+```
+
+* hasIframe:`true` or `false`. 
+
+If you have your own iframe tag,you should set `true`.default`false`.
+
+* body:`true` or `false`.
+
+set `body` tag style overflow,if `{body:true}`,body get `overflow-y: hidden;`.
+
+* width:iframe width,default:`100%`.
+
+* height:iframe height,default:`100%`.
+
+* name:iframe name,default:`responseIframe`.
+
+## some tip
+
+If your address is the same as your iframe,you can add this in your js:
+
+```html
+<!-- your html -->
+<iframe id="response"></iframe>
+```
+
+```javascript
+var iframe = responseIframe("#response","youUrl");
+var iframeElement = document.getElementById("response");
+
+window.addEventListener("load",function(event){
+  var height = iframeElement.contentWindow.document.body.offsetHeight;
+  iframe.changeAttr("height",height+"px");
+});
+```
---
+++ src/response-iframe.js
@@ -0,0 +1,90 @@
+/*
+id在不同情况下代表不同的意思
+1.如果option中参数hasIframe等于true,那么id为那个已经存在iframe的id.
+2.如果option中参数hasIframe等于false,那么id为新iframe需要挂载的地方
+*/
+function responseIframe(id,url,option){
+	if(typeof id==="undefined" || typeof url==="undefined"){
+		console.error("responseIframe need id and url");
+		return;
+	}
+
+	var iframe = {
+		element:"",
+		option:{},
+		changeAttr:function(attr,value){
+			this.element[attr] = value;
+		},
+	};
+	var body = document.querySelector("body");
+
+	// 设置元素src
+	function setUrl(element,newUrl){
+		element.setAttribute("src",newUrl);
+	}
+
+	// 设置其他属性
+	function setAttr(iframe,attrArray){
+		for(var i=0;i<attrArray.length;i++){
+			iframe.element.setAttribute(attrArray[i],iframe.option[attrArray[i]]);
+		}
+	}
+
+	if(typeof option!=="undefined"){
+		// 是否已经有了自己的iframe
+		iframe.option.hasIframe = option.hasIframe?true:false;
+		iframe.option.name = option.name?option.name:"responseIframe";
+		iframe.option.width = option.width?option.width:"100%";
+		iframe.option.height = option.height?option.height:"100%";
+		iframe.option.frameborder = option.frameborder?option.frameborder:"0";
+		iframe.option.sameDomain = option.sameDomain?true:false;
+		iframe.option.body = option.body?true:false;
+	}
+	else{
+		// 否则默认创建一个新的iframe
+		iframe.option.hasIframe = false;
+		iframe.option.name = "responseIframe";
+		iframe.option.width = "100%";
+		iframe.option.height = "100%";
+		iframe.option.frameborder = "0";
+		iframe.option.sameDomain = false;
+		iframe.option.body = false;
+	}
+
+	// 开始搭建一个iframe
+	if(iframe.option.hasIframe){
+		// 已经有一个iframe
+		iframe.element = document.querySelector(id);
+	}
+	else{
+		// 新建一个
+		iframe.element = document.createElement("iframe");
+	}
+
+	setUrl(iframe.element,url);
+	setAttr(iframe,["name","width","height","frameborder"]);
+
+	body.style.height = "100%";
+	body.style.width = "100%";
+	body.style.position = "absolute";
+	body.style.overflowY = iframe.option.body?"hidden":"scroll";
+	body.style.overflowX = "hidden";
+	
+	if(iframe.option.hasIframe){
+		// 有自己的框架
+		iframe.element = document.querySelector(id);
+	}
+	else{
+		// 将创建的iframe挂载到id上
+		document.querySelector(id).style.height = "100%";
+		document.querySelector(id).appendChild(iframe.element);
+	}
+
+	iframe.element.addEventListener("load",function(){
+		if(typeof iframe.option.sameDomain!=="undefined" && iframe.option.sameDomain){
+			iframe.element.height = iframe.element.contentWindow.document.body.offsetHeight;
+		}
+	});
+
+	return iframe;
+}
